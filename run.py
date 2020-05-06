5ef0f1138902183753a34c53f47a37a3dca31571
Tom Tseng <tom.hm.tseng@gmail.com>
2020-05-06 00:30:29 -0700

    run: Add collision logic

--- run.py
+++ run.py
@@ -15,12 +15,45 @@ NUM_CONTROL_DIMENSIONS = 1
 # state for a car is [position, speed]
 NUM_STATE_DIMENSIONS = 2
 
+CAR_MASSES = [1.0, 1.0]  # kg
 CAR_RADIUS = 0.9  # meters
 MAX_ACCELERATION = 3.9  # m/s^2
 # Coefficient of restitution, determining the elasticity of car collisions
 RESTITUTION = 0.1
 
 SECONDS_TO_MILLISECONDS = 1000
+
+
+def add_collision_constraint(solver, prev_state, next_state, car_1, car_2):
+    """Adds solver constraint that next_state is equal to prev_state after a
+    collision between car_1 and car_2."""
+    for i in range(NUM_CARS):
+        if i != car_1 and i != car_2:
+            solver.AddConstraint(pydrake.math.eq(prev_state[i], next_state[i]))
+
+    solver.AddConstraint(prev_state[car_1, 0] == next_state[car_1, 0])
+    solver.AddConstraint(prev_state[car_2, 0] == next_state[car_2, 0])
+
+    mass_1, mass_2 = CAR_MASSES[car_1], CAR_MASSES[car_2]
+    velocity_1, velocity_2 = prev_state[car_1, 1], prev_state[car_2, 1]
+    solver.AddConstraint(
+        next_state[car_1, 1]
+        == (
+            RESTITUTION * mass_2 * (velocity_2 - velocity_1)
+            + mass_1 * velocity_1
+            + mass_2 * velocity_2
+        )
+        / (mass_1 + mass_2)
+    )
+    solver.AddConstraint(
+        next_state[car_2, 1]
+        == (
+            RESTITUTION * mass_1 * (velocity_1 - velocity_2)
+            + mass_1 * velocity_1
+            + mass_2 * velocity_2
+        )
+        / (mass_1 + mass_2)
+    )
 
 
 def plot_trajectory(state_trajectory, goal_state):
@@ -122,8 +155,13 @@ def solve(start_state, goal_state, num_time_samples, collision_sequence=[]):
     num_trajectories = len(collision_sequence) + 1
     time_samples_per_trajectory = num_time_samples // num_trajectories
 
-    # Set the initial state trajectory guess to be a linear interpolation from
-    # the start state to the goal state.
+    # Since this is in one dimension, cars cannot pass each other. Pre-compute
+    # the order of cars for collision checking later.
+    car_order = list(range(NUM_CARS))
+    car_order.sort(key=lambda c: start_state[c, 0])
+
+    # The initial state trajectory guess will be a linear interpolation from the
+    # start state to the goal state.
     TIMESTEP_GUESS = (MAX_TIMESTEP - MIN_TIMESTEP) / 2
     state_trajectory_guess = pydrake.trajectories.PiecewisePolynomial.FirstOrderHold(
         [0.0, (time_samples_per_trajectory * num_trajectories) * TIMESTEP_GUESS],
@@ -164,15 +202,25 @@ def solve(start_state, goal_state, num_time_samples, collision_sequence=[]):
         else:
             # Constrain start of the sub-trajectory to be the result of a
             # collision at the end of the previous trajectory.
-            # TODO(tomtseng)
-            assert False
+            car_1, car_2 = collision_sequence[traj_idx - 1]
+            add_collision_constraint(
+                solver,
+                prev_state=state_vars[-2][-1],
+                next_state=state_vars[-1][0],
+                car_1=car_1,
+                car_2=car_2,
+            )
 
         if traj_idx == num_trajectories - 1:
             solver.AddConstraint(pydrake.math.eq(state_vars[-1][-1], goal_state))
         else:
             # Constrain sub-trajectory to end with a collision.
-            # TODO(tomtseng)
-            assert False
+            car_1, car_2 = collision_sequence[traj_idx]
+            position_1 = state_vars[-1][-1, car_1, 0]
+            position_2 = state_vars[-1][-1, car_2, 0]
+            # NOTE(tomtseng): For the 1-D case, I assume that car_1 hits car_2
+            # from the left.
+            solver.AddConstraint(position_2 == position_1 + 2 * CAR_RADIUS)
 
         for t in range(time_samples_per_trajectory):
             pydrake.systems.trajectory_optimization.AddDirectCollocationConstraint(
@@ -186,10 +234,6 @@ def solve(start_state, goal_state, num_time_samples, collision_sequence=[]):
             )
 
         # Don't allow collisions within a sub-trajectory.
-        # Since this is in one dimension, cars cannot pass each other, so we only
-        # have to check collisions between adjacent cars.
-        car_order = list(range(NUM_CARS))
-        car_order.sort(key=lambda c: start_state[c, 0])
         for i in range(1, NUM_CARS):
             car_prev = car_order[i - 1]
             car_next = car_order[i]
@@ -252,6 +296,7 @@ def solve(start_state, goal_state, num_time_samples, collision_sequence=[]):
             cumulative_time = times[-1]
             state_trajectory.ConcatenateInTime(subtrajectory)
 
+        print("Cost: {}".format(solver_result.get_optimal_cost()))
         plot_trajectory(
             state_trajectory=state_trajectory, goal_state=goal_state,
         )
@@ -269,8 +314,8 @@ START_STATE = np.array([[0, 0], [4, -5]])
 # Don't enforce heading on goal state --- there are undesirable results if the
 # cars turn more than 360 degrees in one direction.
 GOAL_STATE = np.array([[3, 0], [6, 0]])
-NUM_TIME_SAMPLES = 30
-COLLISION_SEQUENCE = []
+NUM_TIME_SAMPLES = 50
+COLLISION_SEQUENCE = [(0, 1)]
 
 solve(
     start_state=START_STATE,
