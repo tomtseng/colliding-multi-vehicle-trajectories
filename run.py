77b530731d90e0402f60e2a0bff6d836b048f00a
Tom Tseng <tom.hm.tseng@gmail.com>
2020-05-05 23:58:47 -0700

    run: Set up scaffolding for hybrid trajectory with collisions

--- run.py
+++ run.py
@@ -83,7 +83,7 @@ def plot_trajectory(state_trajectory, goal_state):
     plt.show()
 
 
-def solve(start_state, goal_state, num_time_samples):
+def solve(start_state, goal_state, num_time_samples, collision_sequence=[]):
     """Solves for vehicle trajectory from the start state to the goal state.
 
     Arguments:
@@ -91,10 +91,18 @@ def solve(start_state, goal_state, num_time_samples):
         Desired start state of the cars.
       goal_state: (NUM_CARS x NUM_STATE_DIMENSIONS)-dimensional array of Optional[float]
         Desired final state of the cars.
+      num_time_samples: int
+        Number of time samples in the solution. Increasing this number will make
+        the solver take longer but will make the output trajectory approximately
+        follow the dynamics and state/control constraints more closely.
+      num_collisions: List[Tuple[int, int]]
+        The collisions that will happen in this trajectory in order. A collision
+        is specified by a pair of cars that will collide.
     """
     MIN_TIMESTEP = 0.01
     MAX_TIMESTEP = 0.5
 
+    # Model the dynamics of the system.
     A = np.kron(np.eye(NUM_CARS), np.array([[0, 1], [0, 0]]))
     B = np.kron(np.eye(NUM_CARS), np.array([[0], [1]]))
     C = np.eye(NUM_STATE_DIMENSIONS * NUM_CARS)
@@ -108,78 +116,109 @@ def solve(start_state, goal_state, num_time_samples):
         context=system_context)
 
     solver = pydrake.solvers.mathematicalprogram.MathematicalProgram()
-    state_vars = solver.NewContinuousVariables(
-        (num_time_samples + 1) * NUM_CARS * NUM_STATE_DIMENSIONS, name="state"
-    ).reshape((num_time_samples + 1, NUM_CARS, NUM_STATE_DIMENSIONS))
-    control_vars = solver.NewContinuousVariables(
-        (num_time_samples + 1) * NUM_CARS * NUM_CONTROL_DIMENSIONS, name="control"
-    ).reshape((num_time_samples + 1, NUM_CARS, NUM_CONTROL_DIMENSIONS))
-    time_vars = solver.NewContinuousVariables(num_time_samples, name="time")
 
-    solver.AddBoundingBoxConstraint(MIN_TIMESTEP, MAX_TIMESTEP, time_vars)
+    # Solve for the trajectory as a sequence of collision-free sub-trajectories.
+    # Each sub-trajectory is a cubic spline.
+    num_trajectories = len(collision_sequence) + 1
+    time_samples_per_trajectory = num_time_samples // num_trajectories
 
-    solver.AddConstraint(pydrake.math.eq(state_vars[0], start_state))
-    solver.AddConstraint(pydrake.math.eq(state_vars[-1], goal_state))
-
-    solver.AddBoundingBoxConstraint(-MAX_ACCELERATION, MAX_ACCELERATION, control_vars[:, :, 0])
-
-    for t in range(num_time_samples):
-        pydrake.systems.trajectory_optimization.AddDirectCollocationConstraint(
-                constraint=collocation_constraint,
-                timestep=[time_vars[t]],
-                state=state_vars[t].flatten(),
-                next_state=state_vars[t + 1].flatten(),
-                input=control_vars[t].flatten(),
-                next_input=control_vars[t + 1].flatten(),
-                prog=solver)
-
-    # Don't allow collisions.
-    # Since this is in one dimension, cars cannot pass each other, so we only
-    # have to check collisions between adjacent cars.
-    car_order = list(range(NUM_CARS))
-    car_order.sort(key=lambda c: start_state[c, 0])
-    for i in range(1, NUM_CARS):
-        car_prev = car_order[i - 1]
-        car_next = car_order[i]
-        position_prev = state_vars[:, car_prev, 0]
-        position_next = state_vars[:, car_next, 0]
-        solver.AddConstraint(pydrake.math.ge(position_next, position_prev + 2 * CAR_RADIUS))
-
-    # Penalize solutions that use a lot of time.
-    solver.AddCost(sum(time_vars))
-
-    # Set the initial state trajectory guess to be  a linear interpolation from
+    # Set the initial state trajectory guess to be a linear interpolation from
     # the start state to the goal state.
     TIMESTEP_GUESS = (MAX_TIMESTEP - MIN_TIMESTEP) / 2
     state_trajectory_guess = pydrake.trajectories.PiecewisePolynomial.FirstOrderHold(
-        [0.0, num_time_samples * TIMESTEP_GUESS],
+        [0.0, (time_samples_per_trajectory * num_trajectories) * TIMESTEP_GUESS],
         np.column_stack((start_state.flatten(), goal_state.flatten())),
     )
-    solver.SetInitialGuess(time_vars, np.full_like(a=time_vars, fill_value=TIMESTEP_GUESS))
-    solver.SetInitialGuess(
-            state_vars.reshape((num_time_samples + 1, -1)),
-            np.vstack([state_trajectory_guess.value(t * TIMESTEP_GUESS).T for t in
-                range(num_time_samples + 1)]))
+
+    state_vars = []  # represents state of cars at each time sample
+    control_vars = []  # represents control of cars at each time sample
+    time_vars = []  # represents interval between each time sample
+    for traj_idx in range(num_trajectories):
+        state_vars.append(solver.NewContinuousVariables(
+            (time_samples_per_trajectory + 1) * NUM_CARS * NUM_STATE_DIMENSIONS,
+            name="state" + str(traj_idx)
+        ).reshape((time_samples_per_trajectory + 1, NUM_CARS, NUM_STATE_DIMENSIONS)))
+        control_vars.append(solver.NewContinuousVariables(
+            (time_samples_per_trajectory + 1) * NUM_CARS * NUM_CONTROL_DIMENSIONS,
+            name="control" + str(traj_idx)
+            ).reshape((time_samples_per_trajectory + 1, NUM_CARS, NUM_CONTROL_DIMENSIONS)))
+        time_vars.append(solver.NewContinuousVariables(time_samples_per_trajectory,
+            name="time" + str(traj_idx)))
+
+        solver.AddBoundingBoxConstraint(MIN_TIMESTEP, MAX_TIMESTEP, time_vars[-1])
+        solver.AddBoundingBoxConstraint(-MAX_ACCELERATION, MAX_ACCELERATION, control_vars[-1][:, :, 0])
+
+        if traj_idx == 0:
+            solver.AddConstraint(pydrake.math.eq(state_vars[-1][0], start_state))
+        else:
+            # Constrain start of the sub-trajectory to be the result of a
+            # collision at the end of the previous trajectory.
+            # TODO(tomtseng)
+            assert False
+
+        if traj_idx == num_trajectories - 1:
+            solver.AddConstraint(pydrake.math.eq(state_vars[-1][-1], goal_state))
+        else:
+            # Constrain sub-trajectory to end with a collision.
+            # TODO(tomtseng)
+            assert False
+
+        for t in range(time_samples_per_trajectory):
+            pydrake.systems.trajectory_optimization.AddDirectCollocationConstraint(
+                    constraint=collocation_constraint,
+                    timestep=[time_vars[-1][t]],
+                    state=state_vars[-1][t].flatten(),
+                    next_state=state_vars[-1][t + 1].flatten(),
+                    input=control_vars[-1][t].flatten(),
+                    next_input=control_vars[-1][t + 1].flatten(),
+                    prog=solver)
+
+        # Don't allow collisions within a sub-trajectory.
+        # Since this is in one dimension, cars cannot pass each other, so we only
+        # have to check collisions between adjacent cars.
+        car_order = list(range(NUM_CARS))
+        car_order.sort(key=lambda c: start_state[c, 0])
+        for i in range(1, NUM_CARS):
+            car_prev = car_order[i - 1]
+            car_next = car_order[i]
+            position_prev = state_vars[-1][:, car_prev, 0]
+            position_next = state_vars[-1][:, car_next, 0]
+            solver.AddConstraint(pydrake.math.ge(position_next, position_prev + 2 * CAR_RADIUS))
+
+        solver.SetInitialGuess(time_vars[-1], np.full_like(a=time_vars[-1], fill_value=TIMESTEP_GUESS))
+        solver.SetInitialGuess(
+                state_vars[-1].reshape((time_samples_per_trajectory + 1, -1)),
+                np.vstack([state_trajectory_guess.value(((time_samples_per_trajectory
+                    * traj_idx) + t) * TIMESTEP_GUESS).T for t in
+                range(time_samples_per_trajectory + 1)]))
+
+    # Penalize solutions that use a lot of time.
+    solver.AddCost(sum(sum(ts) for ts in time_vars))
 
     solver_result = pydrake.solvers.mathematicalprogram.Solve(solver)
-
     if solver_result.is_success():
-        # Get the state trajectory as a cubic spline. This follows the
-        # implementation of
-        # pydrake.systems.trajectory_optimization.DirectCollocation::ReconstructStateTrajectory
-        # from
-        # https://github.com/RobotLocomotion/drake/blob/709ff3ed522f158c4dfbb5c6cfb50190e47af588/systems/trajectory_optimization/direct_collocation.cc#L224
-        timesteps = solver_result.GetSolution(time_vars)
-        states = solver_result.GetSolution(state_vars.reshape((num_time_samples + 1, -1)))
-        controls = solver_result.GetSolution(control_vars.reshape((num_time_samples + 1, -1)))
-        times = np.empty(num_time_samples + 1)
-        derivatives = np.empty_like(states)
-        for t in range(num_time_samples + 1):
-            times[t] = 0 if t == 0 else times[t - 1] + timesteps[t - 1]
-            system_context.SetContinuousState(states[t])
-            system_context.FixInputPort(index=0, data=controls[t])
-            derivatives[t] = linear_system.EvalTimeDerivatives(system_context).CopyToVector()
-        state_trajectory = pydrake.trajectories.PiecewisePolynomial.CubicHermite(breaks=times, samples=states.T, samples_dot=derivatives.T)
+        state_trajectory = pydrake.trajectories.PiecewisePolynomial()
+
+        cumulative_time = 0
+        for traj_idx in range(num_trajectories):
+            # Get the state trajectory as a cubic spline. This follows the
+            # implementation of
+            # pydrake.systems.trajectory_optimization.DirectCollocation::ReconstructStateTrajectory
+            # from
+            # https://github.com/RobotLocomotion/drake/blob/709ff3ed522f158c4dfbb5c6cfb50190e47af588/systems/trajectory_optimization/direct_collocation.cc#L224
+            timesteps = solver_result.GetSolution(time_vars[traj_idx])
+            states = solver_result.GetSolution(state_vars[traj_idx].reshape((time_samples_per_trajectory + 1, -1)))
+            controls = solver_result.GetSolution(control_vars[traj_idx].reshape((time_samples_per_trajectory + 1, -1)))
+            times = np.empty(time_samples_per_trajectory + 1)
+            derivatives = np.empty_like(states)
+            for t in range(time_samples_per_trajectory + 1):
+                times[t] = cumulative_time if t == 0 else times[t - 1] + timesteps[t - 1]
+                system_context.SetContinuousState(states[t])
+                system_context.FixInputPort(index=0, data=controls[t])
+                derivatives[t] = linear_system.EvalTimeDerivatives(system_context).CopyToVector()
+            subtrajectory = pydrake.trajectories.PiecewisePolynomial.CubicHermite(breaks=times, samples=states.T, samples_dot=derivatives.T)
+            cumulative_time = times[-1]
+            state_trajectory.ConcatenateInTime(subtrajectory)
 
         plot_trajectory(
             state_trajectory=state_trajectory, goal_state=goal_state,
@@ -199,7 +238,9 @@ START_STATE = np.array([[0, 0], [4, -5]])
 # cars turn more than 360 degrees in one direction.
 GOAL_STATE = np.array([[3, 0], [6, 0]])
 NUM_TIME_SAMPLES = 30
+COLLISION_SEQUENCE = []
 
 solve(
     start_state=START_STATE, goal_state=GOAL_STATE, num_time_samples=NUM_TIME_SAMPLES,
+    collision_sequence=COLLISION_SEQUENCE
 )
